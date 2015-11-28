b0673ae6b81e5e8bfe46600ec6bdd6a0899948fc
zhaozhidan <805413147@qq.com>
2015-11-28 13:37:14 +0800

    aa

---
+++ My-Microblog/Classes/Main(主要)/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My-Microblog
+//
+//  Created by 赵志丹 on 15/11/28.
+//  Copyright © 2015年 赵志丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
