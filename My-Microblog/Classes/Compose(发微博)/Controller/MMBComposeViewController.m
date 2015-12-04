c0864a41d04d3f14aacb40820f9d8aaf6e945201
zhaozhidan <805413147@qq.com>
2015-12-04 21:28:21 +0800

    共和

--- My-Microblog/Classes/Compose(发微博)/Controller/MMBComposeViewController.m
+++ My-Microblog/Classes/Compose(发微博)/Controller/MMBComposeViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MMBComposeViewController.h"
+#import "MMBAccountTool.h"
+#import "MMBAccount.h"
 
 @interface MMBComposeViewController ()
 
@@ -16,13 +18,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setupNav];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 初始化导航栏
+/**
+ * 初始化导航栏
+ */
+- (void)setupNav{
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(back)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"发布" style:UIBarButtonItemStylePlain target:self action:@selector(compose)];
+    self.navigationItem.rightBarButtonItem.enabled = NO;
+    
+    MMBAccount *account = [MMBAccountTool account];
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.text = [NSString stringWithFormat:@"发微博\n%@", account.name];
+    titleLabel.numberOfLines = 0;
+//    NSAttributedString
+//    NSTextAttachment
+//    titleLabel.attributedText = [NSAttributedString attributedStringWithAttachment:<#(nonnull NSTextAttachment *)#>];
+//    self.navigationItem.titleView = titleLabel;
 }
 
+/**
+ * 发布微博
+ */
+- (void)compose{
+    
+}
+
+- (void)back{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
